6ca607e63166bdab9dec4a070e59ba99353db2c1
cameronmckenzie <cameronctc11@gmail.com>
2025-11-30 01:09:25 -0700

    Initial Commit

---
+++ Hangout Finder App/Hangout_Finder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hangout_Finder_AppApp.swift
+//  Hangout Finder App
+//
+//  Created by McKenzie, Cameron - Student on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Hangout_Finder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hangout Finder AppTests/Hangout_Finder_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hangout_Finder_AppTests.swift
+//  Hangout Finder AppTests
+//
+//  Created by McKenzie, Cameron - Student on 11/30/25.
+//
+
+import Testing
+@testable import Hangout_Finder_App
+
+struct Hangout_Finder_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
