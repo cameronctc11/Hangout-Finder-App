7fe48e93a9745bcf6b707ac38c6713f8310e1e4c
cameronmckenzie <cameronctc11@gmail.com>
2025-12-10 15:21:35 -0700

    good spot to stop. add on later. Pt 2.

---
+++ Hangout Finder App/ImageData.swift
@@ -0,0 +1,18 @@
+//
+//  ImageData.swift
+//  Hangout Finder App
+//
+//  Created by McKenzie, Cameron - Student on 12/10/25.
+//
+
+import SwiftUI
+
+struct ImageData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageData()
+}
