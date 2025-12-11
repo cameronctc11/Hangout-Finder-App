0d1dc197b1737d79838f47cb209fa5b0bd7b9804
cameronmckenzie <cameronctc11@gmail.com>
2025-12-11 11:46:53 -0700

    final.

---
+++ Hangout Finder App/SpotView.swift
@@ -0,0 +1,18 @@
+//
+//  SpotView.swift
+//  Hangout Finder App
+//
+//  Created by McKenzie, Cameron - Student on 12/11/25.
+//
+
+import SwiftUI
+
+struct SpotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpotView()
+}
